53fd709fc103c825ece9b5952cec3a8894b48aed
Danny <boblu27@yahoo.com>
2014-03-18 11:59:05 -0400

    Removing Backend Files

--- GetHired/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from GetHired.models import Offer, Interview, Company, Location
-
-admin.site.register(Offer)
-admin.site.register(Interview)
-admin.site.register(Company)
-admin.site.register(Location)
