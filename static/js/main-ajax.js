79a639fd603dbbe1a06968f77fd2788c571f7ff1
Eliezer Encarnacion <eliezerencarnacion@gmail.com>
2014-03-19 00:18:45 -0400

    Added static folder with js folder

---
+++ static/js/main-ajax.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	
+	/*This event handler is assigned to each of the individual post links in the index page
+	 * if clicked, we prevent the default action, and call the post.get ajax function through the
+	 * Dajaxice library, and with it we pass the id of the post being requested to be displayed
+	 */
+	
+	$('body').on('click', 'a.postLink', function(event) {
+		event.preventDefault();
+		console.log(Dajaxice);
+		Dajaxice.post.get(post_callback, {
+			'post_id' : $(this).attr("href"),
+			'post_type': 1
+		});
+	});
+	
+	function post_callback(data) {
+		$('#content').empty();
+		$('#content').append(data['post']);
+	}
+
+});
