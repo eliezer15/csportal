587bbe3503c45ed74fc6d13a07a44b608fd917a9
Danny <boblu27@yahoo.com>
2014-04-01 22:06:34 -0400

    Fixed Random Tid-Bits of front end. Started Implementation of Filter Saving State. Overhaul of Logo and Footer.

--- static/js/mainfunctions.js
+++ static/js/mainfunctions.js
@@ -1,26 +1,53 @@
 $(document).ready(function() {
+
+	if ($('div.info h1').html() == " Get Hired ") {
+		$('a.hired').addClass('current');
+	}
+
+	var offset = 420;
+	var offsetbottom = $('div.footer').offset().top - 900;
+	console.log(offsetbottom);
+	var duration = 200;
+	jQuery(window).scroll(function() {
+		if (jQuery(this).scrollTop() > offsetbottom) {
+			jQuery('.back-to-top').fadeOut(duration);
+		} else if (jQuery(this).scrollTop() > offset) {
+			jQuery('.back-to-top').fadeIn(duration);
+		} else {
+			jQuery('.back-to-top').fadeOut(duration);
+		}
+
+	});
+
+	jQuery('.back-to-top').click(function(event) {
+		event.preventDefault();
+		jQuery('html, body').animate({
+			scrollTop : 0
+		}, duration);
+		return false;
+	})
 	
-if($('div.info h1').html() == " Get Hired "){
-	$('a.hired').addClass('current');
-} 
+	$('div.filteroptions input.btn').click(function(){
+	$('div.filteroptions input').each(function(){
+	this.checked ? sessionStorage.setItem(this.value, true) : sessionStorage.removeItem(this.value);
+	});
 
-var offset = 220;
-var duration = 500;
-jQuery(window).scroll(function() {
-    if (jQuery(this).scrollTop() > offset) {
-        jQuery('.back-to-top').fadeIn(duration);
-    } else {
-        jQuery('.back-to-top').fadeOut(duration);
-    }
+	$('div.filteroptions select').click(function(event) {
+		console.log(event.currentTarget.value);
+	});
+	});
+	
+	var L = sessionStorage.length;
+	for(var i = 0; i < L; i ++){
+		$('div.filteroptions input.'+sessionStorage.key(i)).prop("checked", true);
+		console.log(sessionStorage.key(i));
+	}
 
-});
-
-jQuery('.back-to-top').click(function(event) {
-    event.preventDefault();
-    jQuery('html, body').animate({scrollTop: 0}, duration);
-    return false;
-})
-
-
+	$('div.newform').find('input').addClass('form-control');
+	$('div.newform').find('input').css('width', '50%');
+	$('div.newform').find('select').addClass('form-control');
+	$('div.newform').find('select').css('width', '50%');
+	$('div.newform').find('textarea').addClass('form-control');
+	$('div.newform').find('textarea').css('width', '50%');
 
 });
