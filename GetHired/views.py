4ac751c20af80c335778054bc7632c7ee11ecb0c
Danny <boblu27@yahoo.com>
2014-04-19 15:35:28 -0400

    Implemented connection of interview to offer

--- GetHired/views.py
+++ GetHired/views.py
@@ -145,12 +145,16 @@ def render_new_post_form(request,post_type,post_id=None):
             context_dict['form'] = form(instance=post)
             context_dict['location_form'] = forms.LocationForm(instance=post.location)
             context_dict['company_form'] = forms.CompanyForm(instance=post.company)
+            if post_type == "interview":
+                context['offer_posts'] = models.Offer.objects.filter(author=request.user)
             context['author'] = post.author
             
             context_dict['header'] = 'Edit '
             context_dict['post_id'] = post_id
         else:
             context_dict['form'] = form()
+            if post_type == "interview":
+                context['offer_posts'] = models.Offer.objects.filter(author=request.user)
             context_dict['location_form'] = forms.LocationForm()
             context_dict['company_form'] = forms.CompanyForm()
             context_dict['header'] = 'Add New '
@@ -202,6 +206,8 @@ def create_post(request, post_type, post_id=None):
             return HttpResponseRedirect(redirecturl)
         else:
             context_dict['form'] = user_form
+            if post_type == "interview":
+                context['offer_posts'] = models.Offer.objects.filter(author=request.user)
             context_dict['post_id'] = str(post_id)
             context_dict['location_form'] = forms.LocationForm({'country':data['country'],'state':data['state'],'city':data['city']})
             context_dict['company_form'] = forms.CompanyForm({'name':data['name']})
